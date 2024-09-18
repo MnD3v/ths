15cb20e06cb88a61660c8f12d899ec1e58a7296a
emanuel dineman djatika <equilibre@equilibre.equilibre>
2024-09-18 19:02:00 +0200

    aleatoire, tout est aleatoire. rien de certain

--- src/app/formations/[link]/page.tsx
+++ src/app/formations/[link]/page.tsx
@@ -47,7 +47,7 @@ const Formation = ({ params }: { params: { link: string } }) => {
                     <div className=' justify-center items-center mb-8 md:my-12 mx-2'>
                         <h1 className='text-center text-2xl md:text-4xl'>FORMATIONS </h1>
 
-                        <h1 className='text-center text-2xl md:text-4xl text-orange-600'>{formation?.title}</h1>
+                        <h1 className='text-center text-xl md:text-4xl text-orange-600'>{formation?.title}</h1>
                     </div>
 
                 </div>
@@ -56,7 +56,8 @@ const Formation = ({ params }: { params: { link: string } }) => {
 
                         <div className='m-2 md:m-9'>
                             <img src={"/formations/" + formation?.icon.split('.')[0] + ".jpg"} alt="" className='h-[200px] sm:h-[300px] md:h-[500px] w-full object-cover' />
-                            <h1 className='font-bricolage text-orange-600 mt-6'>{formation?.title}</h1>
+                            <h1 className='font-bricolage text-2xl md:text-4xl text-orange-600 mt-6'>{formation?.title}</h1>
+                            <div className='h-px w-28 bg-blue-900 my-3'></div>
                             <p>{formation?.full_description}</p>
                             <h2 className='text-orange-600 font-bricolage mt-6'>Débouchés</h2>
                             {
